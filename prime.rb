9803a527b29cc44996cd82868e10d1981856a5e1
Leifur Sigurdsson <leifurerik@gmail.com>
2018-10-19 14:22:53 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+  start = 2
+  if number > 1
+    range (start..number-1).to_a
+    range.none? do |i|
+      number % == 0
+    end
+  else
+    false
+  end
+end
